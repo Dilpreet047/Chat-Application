079c54b47e102af5bc1e91cbf2615a1b030212ab
Dilpreet047 <dilpreetluthra5@gmail.com>
2021-08-23 11:28:24 +0530

    Added date

--- utils/messages.js
+++ utils/messages.js
@@ -5,6 +5,7 @@ function formatMessage(username, text, room){
     return {
         username,
         text,
+        date: moment().format("MMM Do YY"), 
         time: moment().format('h:mm a'),
         room
     }
